2ee60aa2490057d280469fac232b0decba515121
David Nokto <david.nokto@gmail.com>
2023-01-09 15:48:57 +0100

    Completed SeedData

--- University.Data/Data/SeedData.cs
+++ University.Data/Data/SeedData.cs
@@ -8,6 +8,7 @@ using Bogus;
 using Bogus.DataSets;
 using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 using University.Core;
+using System.Globalization;
 
 namespace University.Data.Data
 {
@@ -22,8 +23,58 @@ namespace University.Data.Data
 
             var students = GenerateStudents(50);
             await db.AddRangeAsync(students);
+
+            var courses = GenerateCourses(20);
+            await db.AddRangeAsync(courses);
+
+            var enrollments = GenerateEnrollments(courses, students);
+            await db.AddRangeAsync(enrollments);
+
             await db.SaveChangesAsync();
             
+        }
+
+        private static IEnumerable<Enrollment> GenerateEnrollments(IEnumerable<Course> courses, IEnumerable<Student> students)
+        {
+            var rnd = new Random();
+
+            var enrollments = new List<Enrollment>();
+
+            foreach (var student in students) 
+            {
+                foreach (var course in courses)
+                {
+                    if (rnd.Next(0, 5) == 0)
+                    {
+                        var enrollment = new Enrollment
+                        {
+                            Course = course,
+                            Student = student,
+                            Grade = rnd.Next(1, 6)
+                        };
+                            
+                        enrollments.Add(enrollment);
+                    }
+
+                }
+            }
+
+            return enrollments;
+        }
+
+        private static IEnumerable<Course> GenerateCourses(int numberOfCourses)
+        {
+            var courses = new List<Course>();
+
+            for (int i = 0; i < numberOfCourses; i++)
+            {
+                // "hej jag heter david" => "Hej Jag Heter David"
+                var title = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(faker.Company.Bs());
+                var course = new Course { Title = title };
+                courses.Add(course);
+            }
+
+            return courses;
         }
 
         private static IEnumerable<Student> GenerateStudents(int numberOfStudents)
@@ -35,7 +86,7 @@ namespace University.Data.Data
                 var fName = faker.Name.FirstName();
                 var lName = faker.Name.LastName();
                 var avatar = faker.Internet.Avatar();
-                var email = faker.Internet.Email(fName, lName, "lexicon");
+                var email = faker.Internet.Email(fName, lName, "lexicon.se");
 
                 var student = new Student
                 {
